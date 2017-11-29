8496a2cd8e0d63948f96eea183c0c3106ef5dc75
XieTian <Stevens@Stevens-MacBook-Air.local>
2017-11-29 14:03:07 +0800

    add:Swift 4.0

---
+++ Pods/Target Support Files/Pods-SwiftBugtagsDemo/Pods-SwiftBugtagsDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftBugtagsDemo : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftBugtagsDemo
+@end
---
+++ SwiftBugtagsDemo/SwiftBugtagsDemo-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  SwiftBugtagsDemo-Bridging-Header.h
+//  SwiftBugtagsDemo
+//
+//  Created by WhatsXie on 2017/11/28.
+//  Copyright © 2017年 R.S. All rights reserved.
+//
+
+#ifndef SwiftBugtagsDemo_Bridging_Header_h
+#define SwiftBugtagsDemo_Bridging_Header_h
+
+#import <Bugtags/Bugtags.h>
+
+#endif /* SwiftBugtagsDemo_Bridging_Header_h */
